607926eaf80d3ddb9c2b39f4ec57c3c1484680c6
Akuma157 <ahmed15720010@gmail.com>
2020-11-10 22:41:00 +0200

    Fourth Commit

--- 3rd assignment.cpp
+++ 3rd assignment.cpp
@@ -1,13 +1,12 @@
 #include<iostream> /*input and output library*/
 #include<string> /*string library*/
 using namespace std; /*standard line analyze*/
-main() /*Starting the program*/
-{	/*You will enter your data first*/
-	int UNID, cls, grade, sum;/*reserving my integers that i'll use*/
-	string firstname, surname, Dep, con;/*reserving my strings*/
-	int i=0; /*the loop i*/
-	do {
-		i=i+1;
+int UNID, cls, grade, sum, x, avg;/*reserving my integers that i'll use*/
+string firstname, surname, Dep, con;/*reserving my strings*/
+class AhmedsClass{
+	public:
+		insert(){
+		int i=1; /*the loop i*/
 		cout<<"Enter Your First Name: ";/*what will show on the program screen using cout<<*/cin>>firstname; /*what you'll write will be saved as a string using cin>>*/
 		cout<<"Enter Your Sur Name: ";cin>>surname;
 		cout<<"Enter Your Department(T, A or C): ";cin>>Dep;/*after seeing this message enter one of strings T, A or C*/
@@ -20,16 +19,19 @@ main() /*Starting the program*/
 					sum=sum+grade; /*this will sum the marks values*/
 					i=i+1;
 				}
-		 	cout<<"--------------------------------\n";/*a line just to split the input out of the output then end the line by using "\n"*/
+		avg=sum/8;
+	}
+		print(){
+			cout<<"--------------------------------\n";/*a line just to split the input out of the output then end the line by using "\n"*/
 		 	cout<<"- Your Name: "<<firstname<<" "<<surname<<"\n";/*your name output*/
 		 	cout<<"- Your University ID: "<<UNID;/*your University ID output*/
-		 	cout<<"\n- The Average is: "<<sum/8; /*The Average output*/
+		 	cout<<"\n- The Average is: "<<avg; /*The Average output*/
 		 	cout<<"\n- Your Ranking: "; /*depending on your sum (100-90) shows A, (90-80) shows B, (80-70) shows C, (70-60) shows D, (60-0) shows F*/
-			if ((sum/8>=90)&&(sum/8<=100)) cout<<"A";
-		 	else if ((sum/8>=80)&&(sum/8<=89)) cout<<"B";
-		 	else if ((sum/8>=70)&&(sum/8<=79)) cout<<"C";
-		 	else if ((sum/8>=60)&&(sum/8<=69)) cout<<"D";
-		 	else if ((sum/8>=0)&&(sum/8<60)) cout<<"F";
+			if ((avg>=90)&&(avg<=100)) cout<<"A";
+		 	else if ((avg>=80)&&(avg<=89)) cout<<"B";
+		 	else if ((avg>=70)&&(avg<=79)) cout<<"C";
+		 	else if ((avg>=60)&&(avg<=69)) cout<<"D";
+		 	else if ((avg>=0)&&(avg<60)) cout<<"F";
 		 	else cout<<"Open the program again and enter the right values";/*typing something else it will not work*/
 		 	cout<<"\n- Your Department: "; /*your Department output*/
 		 	switch (cls) /*the class you insert as an integer will show up as one of the below strings using switch()*/
@@ -47,11 +49,24 @@ main() /*Starting the program*/
 		 	else if(Dep=="C") cout<<" Computer ";/*typing "A" shows "Computer"*/
 			else cout<<"";/*typing something else than A, T or C (only in CAPS) will not show anything*/
 			/*the class and the department will show up together*/
-			cout<<"\n--------------------------------\n";
-		 	cout<<"Enter Anything to Continue.\nEnter 'Exit' to Exit.\n";cin>>con;
-		 	if (con=="Exit")
-		 		break; /*if you typed "Exit" the program will break*/
- 		}
-	while (i<=10); /*infinite loop*/
-	return 0;/*all the values should turn to 0*/
+			
+
+		}
+};
+
+main() /*Starting the program*/
+{
+	do{
+	x=x+1;
+	AhmedsClass AhmedsObject;
+	AhmedsObject.insert();
+	AhmedsObject.print();
+	sum=0;
+	cout<<"\n--------------------------------\n";
+	cout<<"Enter Anything to Continue.\nEnter 'Exit' to Exit.\n";cin>>con;
+	if (con=="Exit")
+	break; /*if you typed "Exit" the program will break*/
+	}
+	while (x<=10);
+	return 0;
 }
